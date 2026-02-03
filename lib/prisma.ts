cf64eaac8441eba07c68fb966ab88c220f5d8efa
sososmog <sososmog@outlook.com>
2026-02-02 22:50:05 -0500

    Fix: Use any type for D1Database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaD1 } from '@prisma/adapter-d1'
 
-export function getPrismaClient(db: D1Database) {
+// 消除any警告
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function getPrismaClient(db: any) {
   const adapter = new PrismaD1(db)
   return new PrismaClient({ adapter })
 }
